ecc04574264edc45f8fda5ab05c483ef1cbeeb02
VogelDev <rvogel@mc3.edu>
2016-06-10 00:18:36 -0400

    Project Conversion
    Started implementing project changes for task support

--- MemoPad/app/src/main/java/me/vogeldev/memopad/NoteEditActivity.java
+++ MemoPad/app/src/main/java/me/vogeldev/memopad/NoteEditActivity.java
@@ -6,20 +6,33 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import me.vogeldev.memopad.data.NotepadContract;
 
 /**
  * Created by Vogel on 6/9/2016.
  */
-public class NoteEditActivity extends AppCompatActivity {
+public class NoteEditActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     public static final String ACTION_NEW = "action_new";
     public static final String ACTION_EDIT = "action_edit";
+
+    private Spinner spinnerType;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> tasks;
 
     private EditText etTitle;
     private EditText etDesc;
@@ -64,6 +77,30 @@ public class NoteEditActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         // This has the potential to fail, but we are explicitly setting the actionbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tasks = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, tasks);
+
+        ListView listView = (ListView)findViewById(R.id.listView_task);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_task);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tasks.add(etDesc.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // Create array adapter for unit spinners based on string array in the strings.xml file
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.edit_types));
+        spinAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerType = (Spinner)findViewById(R.id.spinnerType);
+        spinnerType.setAdapter(spinAdapter);
     }
 
     @Override
@@ -92,5 +129,11 @@ public class NoteEditActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         save();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        tasks.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
